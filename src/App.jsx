2de380d154ae724ca77c03d19ffd54b3802040b2
anwarmohammadi1390 <anwarmohammadi1390@gmail.com>
2024-11-13 11:39:43 -0800

    Setup the project

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='text-center'>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis architecto minima deserunt ducimus reprehenderit, illum, consequatur ratione earum sequi dolore optio inventore, qui ipsa. Tempora eos totam quasi natus ex?</p>
+    </div>
     </>
   )
 }
