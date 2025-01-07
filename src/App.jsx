55b9eeb4c30eb6b0fe2bb6196242c3e1b6f53d12
AnwarMohammadi2001 <anwarmohammadi1390@gmail.com>
2025-01-06 22:48:02 -0800

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-    <div className='text-center'>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis architecto minima deserunt ducimus reprehenderit, illum, consequatur ratione earum sequi dolore optio inventore, qui ipsa. Tempora eos totam quasi natus ex?</p>
+    <div>
+      
     </div>
-    </>
   )
 }
 
